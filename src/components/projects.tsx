7dda077a837b1224105ed347809ba83128a71735
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-08-20 18:35:01 +0000

    initial commit

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,11 +1,14 @@
 "use client";
 import React from "react";
-import NumberCounter from "number-counter";
 import Image from "next/image";
+import { GraphicDes, Illustrations, UI } from "../../constants";
 
 const Projects = () => {
   return (
-    <div id="projects" className="flex justify-center gap-10 my-10 flex-wrap px-10">
+    <div
+      id="projects"
+      className="flex justify-center gap-10 my-10 flex-wrap px-10"
+    >
       <div className="mb-10 flex-1">
         <h2 className="text-3xl text-center md:text-left mb-3 md:mb-10">
           Projects
@@ -14,21 +17,21 @@ const Projects = () => {
           <div className="bg-gradient-to-r from-[#0383BD] to-[#020F16] text-white rounded-3xl align-middle flex flex-col p-5 items-center justify-center w-full h-[150px] md:h-[250px] md:w-[250px]">
             <h2 className="mb-auto">GRAPHIC DESIGNS</h2>
             <div className="flex justify-center items-center text-5xl flex-1">
-              <NumberCounter end={50} delay={4} />
+              <p>{GraphicDes.length}</p>
               <p className="font-thin">+</p>
             </div>
           </div>
           <div className=" bg-gradient-to-r from-[#7C4DFF] to-[#0c0324] text-white rounded-3xl align-middle flex flex-col p-5 items-center justify-center w-full h-[150px] md:h-[250px] md:w-[250px]">
             <h2 className="mb-auto">UI / UX DESIGNS</h2>
             <div className="flex justify-center items-center text-5xl flex-1">
-              <NumberCounter end={10} delay={4} />
+              <p>{UI.length}</p>
               <p className="font-thin">+</p>
             </div>
           </div>
           <div className=" bg-gradient-to-r from-[#FF5722] to-[#1C0802] text-white rounded-3xl align-middle flex flex-col p-5 items-center justify-center w-full h-[150px] md:h-[250px] md:w-[250px]">
             <h2 className="mb-auto">ILLUSTRATORS</h2>
             <div className="flex justify-center items-center text-5xl flex-1">
-              <NumberCounter end={2} delay={4} />
+              <p>{Illustrations.length}</p>
               <p className="font-thin">+</p>
             </div>
           </div>
