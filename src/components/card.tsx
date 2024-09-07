995f9f2aecd971421b479f69595f091306c0cc16
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-09-07 13:48:03 +0000

    ui fixes

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,16 +2,23 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { BiPhone } from "react-icons/bi";
-import { BsGithub, BsInstagram, BsLinkedin, BsTwitterX, BsWhatsapp, BsX } from "react-icons/bs";
+import {
+  BsGithub,
+  BsInstagram,
+  BsLinkedin,
+  BsTwitterX,
+  BsWhatsapp,
+  BsX,
+} from "react-icons/bs";
 import { FaPinterest } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 import { CardInterface } from "../../types";
 
 const Card = ({ info }: { info: CardInterface }) => {
   return (
-    <div className=" p-5 w-full h-full max-w-sm min-w-36 bg-white rounded-lg overflow-hidden shadow-lg flex flex-col gap-5">
+    <div className=" p-5 w-full h-full max-w-sm min-w-36 bg-white rounded-lg overflow-hidden shadow-lg flex flex-col gap-3">
       <div className="flex gap-5 items-center w-full">
-        <div className="relative min-w-28 min-h-28 ">
+        <div className="relative min-w-20 min-h-20 ">
           <Image
             fill
             src={info?.src}
@@ -21,8 +28,8 @@ const Card = ({ info }: { info: CardInterface }) => {
         </div>
 
         <div className="flex flex-col gap-2">
-          <div className="text-sm text-gray-700 capitalize">{info?.name}</div>
-          <div className="text-sm text-gray-500 capitalize">{info?.role}</div>
+          <div className="text-xs text-gray-700 capitalize">{info?.name}</div>
+          <div className="text-xs text-gray-500 capitalize">{info?.role}</div>
         </div>
       </div>
       {/* <div className="flex justify-between gap-2">
@@ -67,25 +74,23 @@ const Card = ({ info }: { info: CardInterface }) => {
       </div> */}
 
       <div>
-        <h3 className="text-black font-bold">Bio</h3>
-        <div className="text-gray-700 text-xs py-3">
-            {info?.bio}
-        </div>
+        <h3 className="text-black font-bold text-sm">Bio</h3>
+        <div className="text-gray-700 text-xs mt-3">{info?.bio}</div>
       </div>
       <div className="text-black">
-        <h3 className=" font-bold">Contact</h3>
+        <h3 className=" font-bold text-sm">Contact</h3>
         <div className="flex flex-col gap-3 justify-center text-xs">
           {/* mail */}
           {info?.mail && (
             <div className="flex gap-2 items-center">
-              <MdEmail size={20} />
+              <MdEmail size={15} />
               <p>{info?.mail}</p>
             </div>
           )}
           {/* phoneNumber */}
           {info?.phoneNumber && (
             <div className="flex gap-2 items-center">
-              <BiPhone size={20} color="blue" />
+              <BiPhone size={15} color="blue" />
               <Link href={`tel: ${info?.phoneNumber}`}>
                 {info?.phoneNumber}
               </Link>
@@ -94,7 +99,7 @@ const Card = ({ info }: { info: CardInterface }) => {
           {/* whatsapp */}
           {info?.whatsapp && (
             <div className="flex gap-2 items-center">
-              <BsWhatsapp size={20} color="green" />
+              <BsWhatsapp size={15} color="green" />
               <Link target="_blank" href={`https://wa.me/${info?.whatsapp}`}>
                 Whatsapp
               </Link>
@@ -103,11 +108,8 @@ const Card = ({ info }: { info: CardInterface }) => {
           {/* github */}
           {info?.github && (
             <div className="flex gap-2 items-center">
-              <BsGithub size={20} color="" />
-              <Link
-                target="_blank"
-                href={`https://github.com/${info?.github}`}
-              >
+              <BsGithub size={15} color="" />
+              <Link target="_blank" href={`https://github.com/${info?.github}`}>
                 Github
               </Link>
             </div>
@@ -115,11 +117,8 @@ const Card = ({ info }: { info: CardInterface }) => {
           {/* linkedin */}
           {info?.linkedin && (
             <div className="flex gap-2 items-center">
-              <BsLinkedin size={20} color="#0a66c2" />
-              <Link
-                target="_blank"
-                href={info?.linkedin}
-              >
+              <BsLinkedin size={15} color="#0a66c2" />
+              <Link target="_blank" href={info?.linkedin}>
                 Linkedin
               </Link>
             </div>
@@ -127,7 +126,7 @@ const Card = ({ info }: { info: CardInterface }) => {
           {/* pinterest */}
           {info?.pinterest && (
             <div className="flex gap-2 items-center">
-              <FaPinterest size={20} color="red" />
+              <FaPinterest size={15} color="red" />
               <Link target="_blank" href="https://pin.it/gWpzlCHrT">
                 Pinterest
               </Link>
@@ -135,7 +134,7 @@ const Card = ({ info }: { info: CardInterface }) => {
           )}
           {info?.twitter && (
             <div className="flex gap-2 items-center">
-              <BsTwitterX size={20} color="black" />
+              <BsTwitterX size={15} color="black" />
               <Link target="_blank" href={info?.twitter}>
                 Twitter
               </Link>
@@ -143,7 +142,7 @@ const Card = ({ info }: { info: CardInterface }) => {
           )}
           {info?.instagram && (
             <div className="flex gap-2 items-center">
-              <BsInstagram size={20} color="red" />
+              <BsInstagram size={15} color="red" />
               <Link target="_blank" href={info?.instagram}>
                 Instagram
               </Link>
